92dfe81c3040a830aebf4e6311dac2b13087125a
kevin <kevinG>
2026-05-23 16:38:09 -0500

    subir enrutamiento de referencias

--- src/router/index.routes.js
+++ src/router/index.routes.js
@@ -1,5 +1,33 @@
-const enreutanamiento = (router) =>{
-    console.log(router)
+import home from "../Views/Referencias";
+
+const rout = document.getElementById('master');
+
+const enreutanamiento = (router) => {
+    rout.innerHTML = '';
+
+    switch (router) {
+        // Esto atrapa cualquier variante del inicio (vacío, solo #, o #/)
+        case "#/": {  // <--- AGREGA ESTA LÍNEA AQUÍ
+           console.log("alerta roja")
+           break
+        }
+        case '#/Referencias': {
+            return rout.appendChild(home());
+        }
+        case '#/Publicaciones': {
+            console.log('publicaciones');
+            break
+        }
+        case '#/Ubicacion': {
+            console.log('ubicacion');
+            break
+        }
+        default: {
+            rout.innerHTML = '<h2>Error 404: Not Found</h2>';
+            console.log('error 404: not found. Ruta recibida:', router);
+
+        }
+    }
 }
 
-export {enreutanamiento}
+export { enreutanamiento };
