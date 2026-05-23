bedfdbd0cff90504f13d3bad0687f21c560453df
kevin <kevinG>
2026-05-23 18:24:30 -0500

    subir cambios

--- src/controller/index.controller.js
+++ src/controller/index.controller.js
@@ -1,8 +1,18 @@
 //Importaciones de las vistas 
 import Views from "../Views/Referencias.html";
+
 //----------------------------
 export default () => {
+
       const Viweshome = document.createElement('div');
+
       Viweshome.innerHTML = Views;
+
+      const click = Viweshome.querySelector('#Btn_enviar');
+
+      click.addEventListener('click', () => {
+            alert('Esta es una alerta MODOD PRUEBA');
+      });
+
       return Viweshome;
 }
