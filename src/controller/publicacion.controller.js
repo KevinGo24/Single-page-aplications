f4832893e9bb2ff0a18ddac24926a455714cbf80
kevin <kevin@gmail.com>
2026-05-26 12:24:05 -0500

    agregar cambios en el controller de publicacion

--- src/controller/publicacion.controller.js
+++ src/controller/publicacion.controller.js
@@ -1,14 +1,29 @@
 import post from "../Views/publicacion.html"
-const getpost =  async () =>{
-   const reponse = await fetch('https://jsonplaceholder.typicode.com/users')
-   return await reponse.json()
+
+const getpost = async () => {
+    const reponse = await fetch('https://jsonplaceholder.typicode.com/posts')
+    return await reponse.json()
 }
-export default async () =>{
+
+export default async () => {
+
     const post_views = document.createElement('div')
+
     post_views.innerHTML = post
-    const elementpost =  post_views.querySelector('#post')
-    getpost.foreach(post =>{
-        post_views.innerHTML += 
+
+    const elementpost = post_views.querySelector('#post')
+
+    const reponse_post = await getpost()
+
+    reponse_post.forEach(post => {
+
+        elementpost.innerHTML += `
+        <li style="list-style: none;color:white">
+            <h5>${post.title}</h5>
+            <p>${post.body}</p>
+        </li>
+        `
     })
+
     return post_views
 }
